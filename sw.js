999d99dc4433ff3218bb95599c67a4eea32c505d
Dimitris <zdimitris@outlook.com>
2021-06-11 16:12:01 +0300

    Added service workers and manifest

---
+++ sw.js
@@ -0,0 +1,14 @@
+'use strict';
+
+self.addEventListener('install', () => {
+    console.log(`installing service worker`);
+})
+
+self.addEventListener('activate', () => {
+    console.log(`activating service worker`);
+})
+
+self.addEventListener('fetch', event => {
+    console.log(`fetching...
+    ${event.request.url}`);
+})
