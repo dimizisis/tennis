8bbc3e49cb2f68947d3c366b37339bfb8811f690
Dimitris <zdimitris@outlook.com>
2021-06-11 16:32:05 +0300

    Service workers fix

--- sw.js
+++ sw.js
@@ -1,14 +1,32 @@
-'use strict';
+self.addEventListener('install', function (event) {
+    console.log('The service worker is being installed.');
+    event.waitUntil(
+        caches.open('tennis').then(function(cache) {
+            return cache.addAll([
+                '/index.html',
+                '/images/icons/favicon.ico',
+                '/manifest.json',
+                '/images/icons/facebook.svg',
+                '/images/icons/instagram.svg',
+                '/images/icons/info.svg',
+                '/images/icons/invisible.svg',
+                '/images/icons/plus.svg',
+                '/images/icons/ranking.svg',
+                '/images/icons/remove.svg',
+                '/images/icons/stats.svg',
+                '/images/icons/twitter.svg',
+                '/images/icons/visible.svg',
+                '/js/index.js'
+            ]);
+        })
+    );
+});
 
-self.addEventListener('install', () => {
-    console.log(`installing service worker`);
-})
-
-self.addEventListener('activate', () => {
-    console.log(`activating service worker`);
-})
-
-self.addEventListener('fetch', event => {
-    console.log(`fetching...
-    ${event.request.url}`);
-})
+self.addEventListener('fetch', function (event) {
+    console.log('The service worker is serving the asset.');
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response || caches.match('/index.html');
+        })
+    );
+});
