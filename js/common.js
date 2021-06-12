ac88695f2bb37724ba1a24d14b8f8c60a288d73b
Dimitris <zdimitris@outlook.com>
2021-06-12 16:19:09 +0300

    Update common.js

--- js/common.js
+++ js/common.js
@@ -2,7 +2,7 @@
 
 // Service Worker
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js').catch(function (error) {
+    ServiceWorkerContainer.register('/sw.js').catch(function (error) {
         // registration failed
         console.log('registration failed');
     });
