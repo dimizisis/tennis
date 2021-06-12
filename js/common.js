5cb34c8189ab16fab58ddeb65d8adbb9974c8c09
Dimitris <zdimitris@outlook.com>
2021-06-12 18:24:49 +0300

    Update common.js

--- js/common.js
+++ js/common.js
@@ -15,34 +15,35 @@ if ('serviceWorker' in navigator) {
 let deferredPrompt;
 const installBtn = document.getElementById('install');
 
-window.addEventListener('beforeinstallprompt', function (ev) {
-    // Prevent some older browsers from popping the install prompt
-    ev.preventDefault();
+window.addEventListener('beforeinstallprompt', (event) => {
+    console.log('👍', 'beforeinstallprompt', event);
     // Stash the event so it can be triggered later.
-    deferredPrompt = ev;
-    // Update UI to notify the user they can add to home screen
-    installBtn.style.visibility = 'visible';
+    window.deferredPrompt = event;
+    // Remove the 'hidden' class from the install button container
+    installBtn.classList.toggle('hidden', false);
+  });
 
-    installBtn.addEventListener('click', function () {
-        console.log('clicked');
-        // Show the prompt
-        deferredPrompt.prompt();
-        // Wait for the user to respond to the prompt
-        deferredPrompt.userChoice.then(function (choiceResult) {
-            if (choiceResult.outcome === 'accepted') {
-                // Don't need it any more
-                installBtn.style.visibility = 'hidden';
-                deferredPrompt = null;
-                console.log('User accepted the A2HS prompt');
-            } else {
-                console.log('User dismissed the A2HS prompt');
-            }
-        });
-    });
+installBtn.addEventListener('click', async () => {
+    console.log('👍', 'butInstall-clicked');
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+        // The deferred prompt isn't available.
+        return;
+    }
+    // Show the install prompt.
+    promptEvent.prompt();
+    // Log the result
+    const result = await promptEvent.userChoice;
+    console.log('👍', 'userChoice', result);
+    // Reset the deferred prompt variable, since
+    // prompt() can only be called once.
+    window.deferredPrompt = null;
+    // Hide the install button.
+    installBtn.classList.toggle('hidden', true);
 });
 
-window.addEventListener('appinstalled', function () {
-    installBtn.style.visibility = 'hidden';
-    deferredPrompt = null;
-    console.log('PWA was installed');
+window.addEventListener('appinstalled', (event) => {
+    console.log('👍', 'appinstalled', event);
+    // Clear the deferredPrompt so it can be garbage collected
+    window.deferredPrompt = null;
 });
