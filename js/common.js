90b185c52e20634c5cb15a2a23a4acaadf3460ca
Dimitris <zdimitris@outlook.com>
2021-06-12 17:04:13 +0300

    Update index.html

--- js/common.js
+++ js/common.js
@@ -13,21 +13,35 @@ if ('serviceWorker' in navigator) {
 
 // Handle A2HS
 let deferredPrompt;
+const installBtn = document.getElementById('install');
 
 window.addEventListener('beforeinstallprompt', function (ev) {
-
-    console.log('triggered beforeinstallprompt');
+    // Prevent some older browsers from popping the install prompt
     ev.preventDefault();
+    // Stash the event so it can be triggered later.
     deferredPrompt = ev;
-    deferredPrompt.prompt();
+    // Update UI to notify the user they can add to home screen
+    installBtn.style.visibility = 'visible';
 
-    deferredPrompt.userChoice.then(function (choiceResult) {
-        if (choiceResult.outcome === 'accepted')
-            deferredPrompt = null;
+    installBtn.addEventListener('click', function () {
+        // Show the prompt
+        deferredPrompt.prompt();
+        // Wait for the user to respond to the prompt
+        deferredPrompt.userChoice.then(function (choiceResult) {
+            if (choiceResult.outcome === 'accepted') {
+                // Don't need it any more
+                installBtn.style.visibility = 'hidden';
+                deferredPrompt = null;
+                console.log('User accepted the A2HS prompt');
+            } else {
+                console.log('User dismissed the A2HS prompt');
+            }
+        });
     });
 });
 
 window.addEventListener('appinstalled', function () {
+    installBtn.style.visibility = 'hidden';
     deferredPrompt = null;
-    console.log('app installed');
+    console.log('PWA was installed');
 });
